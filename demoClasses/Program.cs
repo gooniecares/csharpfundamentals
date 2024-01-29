41d4b61314a93ebb19f458ed22d0c493a4f2e2ae
Benjamin Carter <benjamincarter@caresolutions.com>
2024-01-29 02:51:44 -0500

    All On File

--- demoClasses/Program.cs
+++ demoClasses/Program.cs
@@ -6,10 +6,24 @@ using System.Threading.Tasks;
 
 namespace demoClasses
 {
+    public class Person
+    {
+        public string FirstName;
+        public string LastName;
+
+        public void Introduce()
+        {
+            Console.WriteLine("My name is " + FirstName + " " + LastName);
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
+            var ben = new Person();
+            ben.FirstName = "Benjamin";
+            ben.LastName = "Carter";
+            ben.Introduce();
         }
     }
 }
