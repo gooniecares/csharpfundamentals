987af9673fe8713f58cb033ec08124ed6287c1b2
Benjamin Carter <benjamincarter@caresolutions.com>
2024-01-29 03:22:20 -0500

    Switch On Time of int 10 or 14

---
+++ IfEsleSwitchCase/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IfEsleSwitchCase
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int hour = 14;
+
+            if(hour > 0 && hour < 12) 
+            {
+                Console.WriteLine("Its Moring");
+            }
+            else if(hour > 0 && hour < 12)
+            {
+                Console.WriteLine("Its AfterNoon");
+            }
+            else
+            {
+                Console.WriteLine("It's Evening");
+            }
+            
+        }
+    }
+}
