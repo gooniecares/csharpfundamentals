7093202d6c618268ce2865609f76c790c57d4583
Benjamin Carter <benjamincarter@caresolutions.com>
2024-01-29 02:29:50 -0500

    Second Commit

--- capp1/Program.cs
+++ capp1/Program.cs
@@ -12,9 +12,24 @@ namespace capp1
         {
             var a = 10;
             var b = 3;
+            var c = 5;
 
-            //Console.WriteLine(a + b);
-            //Console.WriteLine((float)a / (float)b);
+            //Console.WriteLine(a + b);                             //Yiekds 13
+            //Console.WriteLine((float)a / (float)b);               //Yeikds 3.333333
+            //Console.WriteLine(a + b + c);                         //Yeilds 18
+            //Console.WriteLine(a + b * c);                         //Yeilds 25
+
+            //Console.WriteLine(a > b);                             //Yeilds true
+            //Console.WriteLine(a < b);                             //Yeilds false
+            //Console.WriteLine(a == b);                            //Yeilds false
+            //Console.WriteLine(!(a != b));                         //Yeilds false  2 negatives equal one a positive
+
+            //Console.WriteLine(c < b && c > a);                    //Yeilds false
+            //Console.WriteLine(c > b && c < a);                    //Yeilds true
+            //Console.WriteLine(c > b && c == a);                   //Yeilds false  true and false results to false
+            //Console.WriteLine(c > b || c == a);                   //Yeilds true
+            //Console.WriteLine(c < b || c == a);                   //Yeilds false
+            Console.WriteLine(!(c < b) || c == a);                  //Yeilds true
 
 
         }
