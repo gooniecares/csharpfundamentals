f8e436c9b8143c160e3aa144aa22a91b2668ff41
Benjamin Carter <benjamincarter@caresolutions.com>
2024-01-29 02:16:37 -0500

    First Commit

---
+++ capp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace capp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 10;
+            var b = 3;
+
+            //Console.WriteLine(a + b);
+            //Console.WriteLine((float)a / (float)b);
+
+
+        }
+    }
+}
