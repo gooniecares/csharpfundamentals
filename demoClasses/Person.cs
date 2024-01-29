81f22b93da0f920c77a5d2fb5dbb1e8e1b49e74b
Benjamin Carter <benjamincarter@caresolutions.com>
2024-01-29 02:59:51 -0500

    Moved Person To new File Referenced By Blass

---
+++ demoClasses/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace demoClasses
+{
+    internal class Person
+    {
+        public string FirstName { get; internal set; }
+        public string LastName { get; internal set; }
+
+        internal void Introduce()
+        {
+            Console.WriteLine("My name is " + FirstName + " " + LastName);
+        }
+
+       
+    }
+}
