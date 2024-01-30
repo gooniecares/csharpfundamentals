f0969ecc4ab61cd6dfb2a4980aa57b5900bcbeb6
Benjamin Carter <benjamincarter@caresolutions.com>
2024-01-30 01:17:21 -0500

    For Each Loops Demo

---
+++ ForEachLoops/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForEachLoops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // The goal is to show each character on each line.
+            // Both for loops are doing the same thing.
+
+            //var name = "John Smith";
+
+            //for (var i = 0; i < name.Length; i++)
+            //{
+            //    Console.WriteLine(name[i]);
+            //}
+
+            //foreach(var character in name)
+            //{
+            //    Console.WriteLine(character);
+            //}
+
+            var numbers = new int[] { 1, 2, 3, 4, 5 };
+
+            foreach (var number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+
+        }
+    }
+}
